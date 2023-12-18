88845b9e6319d3020804fd7ff8e71c0a285f63ce
Leo Immonen <leo.j.immonen99@gmail.com>
2023-12-18 05:40:44 +0200

    Updates

--- _app/immutable/chunks/paths.c7B-pelF.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_14em70e?.base??"/kimble",e=globalThis.__sveltekit_14em70e?.assets??s;export{e as a,s as b};
---
+++ _app/immutable/chunks/paths.ePqxBt5P.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_196anaq?.base??"/kimble",a=globalThis.__sveltekit_196anaq?.assets??s;export{a,s as b};
