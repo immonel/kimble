dd609b4fb30a75e4406894f7c9f7032fecad9766
Leo Immonen <leo.j.immonen99@gmail.com>
2023-12-18 06:00:13 +0200

    Redirect to front page

--- src/components/BackButton.svelte
+++ src/components/BackButton.svelte
@@ -1,5 +1,10 @@
+<script>
+  import { goto } from "$app/navigation";
+  import { base } from "$app/paths";
+</script>
+
 <button
-  on:click={() => history.back()}
+  on:click={() => goto(`${base}/`)}
   class="
     rounded-lg p-1 w-12 h-12
     bg-gray-200 dark:bg-gray-800
